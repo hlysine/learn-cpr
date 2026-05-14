0b73b870a431b32f82cee30cb46e6c4f11165406
Henry Lin <henry.ys.lin@gmail.com>
2026-05-14 10:34:45 +0800

    Respect port from Railway

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,9 @@ import tailwindcss from '@tailwindcss/vite'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 
 const config = defineConfig({
+  server: {
+    port: Number(process.env.PORT) || 3000,
+  },
   resolve: { tsconfigPaths: true },
   plugins: [
     devtools(),
